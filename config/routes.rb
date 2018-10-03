1bd87c90f2d8904417f43c5ede00fe74aaadb794
Tim DiSabatino <tim@pathwaysdigital.com>
2018-10-03 06:46:12 -0700

    Create Project model & bind create project form to model via js

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'project/index'
+  get 'projects', to: 'project#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'projects', to: 'project#create'
 end
